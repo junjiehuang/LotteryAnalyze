0f265511e74aa7ea97e720bf46155c8a10de9446
junjiehuang <2441274617@qq.com>
2018-01-29 20:16:13 +0800

    trade data

---
+++ LotteryAnalyze/TradeData.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LotteryAnalyze
+{
+    public enum TradeType
+    {
+        eNone,
+        eOneStar,
+        eTenSingleTwoStar,
+        eHundredTenTwoStar,
+        eThousandHundredTwoStar,
+        eTenThousandHundredTwoStar,
+        eHundredTenSingleThreeStar,
+        eThousandHundredTenThreeStart,
+        eTenThousandThousandHundredThreeStart,
+        eFiveStar,
+    }
+
+    public enum TradeStatus
+    {
+        eWaiting,
+        eDone,
+    }
+
+    class TradeNumbers
+    {
+        public List<byte> tradeNumbers = new List<byte>();
+        public int tradeCount = 0;
+    }
+
+    class TradeDataBase
+    {
+        public TradeStatus tradeStatus = TradeStatus.eWaiting;
+        public TradeType tradeType = TradeType.eNone;
+        public int dayID = -1;
+        public int lotteryItemID = -1;
+        public DataItem lotteryItem = null;
+
+        public float reward = 0;
+        public float cost = 0;
+
+        public virtual void Update() { }
+    }
+
+    class TradeDataOneStar : TradeDataBase
+    {
+        public static float SingleTradeCost = 1;
+        public static float SingleTradeReward = 9.8f;
+
+        public Dictionary<int, TradeNumbers> tradeInfo = new Dictionary<int, TradeNumbers>();
+
+
+        public TradeDataOneStar()
+        {
+            tradeType = TradeType.eOneStar;
+        }
+
+        public override void Update()
+        {
+            if(tradeStatus == TradeStatus.eWaiting)
+            {
+                DataManager DM = DataManager.GetInst();
+                if(DM.allDatas.ContainsKey(dayID))
+                {
+                    OneDayDatas odd = DM.allDatas[dayID];
+                    if(odd.datas.Count > lotteryItemID && lotteryItemID >= 0)
+                    {
+                        reward = 0;
+                        cost = 0;
+                        lotteryItem = odd.datas[lotteryItemID];
+                        foreach( int numIndex in tradeInfo.Keys)
+                        {
+                            TradeNumbers tns = tradeInfo[numIndex];
+                            byte dstValue = lotteryItem.GetNumberByIndex(numIndex);
+                            if(tns.tradeNumbers.Contains(dstValue))
+                                reward += SingleTradeReward * tns.tradeCount;
+                            cost += SingleTradeCost * tns.tradeCount;
+                        }
+                        TradeDataManager.Instance.currentMoney += reward - cost;
+                        tradeStatus = TradeStatus.eDone;
+                    }
+                }
+            }
+        }
+    }
+
+
+    class TradeDataManager
+    {
+        static TradeDataManager sInst = null;
+        public List<TradeDataBase> historyTradeDatas = new List<TradeDataBase>();
+        public List<TradeDataBase> waitingTradeDatas = new List<TradeDataBase>();
+        public float currentMoney = 0;
+
+
+        TradeDataManager()
+        {
+        }
+        public static TradeDataManager Instance
+        {
+            get
+            {
+                if (sInst == null)
+                    sInst = new TradeDataManager();
+                return sInst;
+            }
+        }
+
+        public void Update()
+        {
+            if(waitingTradeDatas.Count > 0)
+            {
+                for( int i = waitingTradeDatas.Count-1; i >= 0; --i )
+                {
+                    waitingTradeDatas[i].Update();
+                    if (waitingTradeDatas[i].tradeStatus == TradeStatus.eDone)
+                    {
+                        historyTradeDatas.Add(waitingTradeDatas[i]);
+                        waitingTradeDatas.RemoveAt(i);
+                    }
+                }
+            }
+        }
+    }
+}
